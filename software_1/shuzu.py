43215dc2517d95651e1b4ab0684467811ea45b25
Chenvh <995440275@qq.com>
2022-03-07 09:40:24 +0800

    first commit

---
+++ software_1/shuzu.py
@@ -0,0 +1,14 @@
+# -*- coding: UTF-8 -*-
+
+num = [1, -2, 3, 5, -1]
+len_num = len(num)
+max = num[0]
+print(max)
+for i in range(len_num) :
+    for j in range(i,)
+    print(num[i])
+    
+        # if max < tmp :
+        #    max = tmp
+
+print(max)
